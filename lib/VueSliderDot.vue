f2dbd71d5fa8c0b22cafcf444eaaea5b0de4ee26
Eiryyy <xxx.eiryklav@gmail.com>
2022-04-08 00:00:32 +0900

    refactor: add VueSliderDot

---
+++ lib/VueSliderDot.vue
@@ -0,0 +1,111 @@
+<script lang="ts" setup>
+import { Value, Styles, Position, TooltipProp, TooltipFormatter } from './typings'
+import './styles/dot.scss'
+import { computed } from 'vue'
+
+const props = withDefaults(
+  defineProps<{
+    value: Value
+    tooltip: TooltipProp
+    dotStyle?: Styles
+    tooltipStyle?: Styles
+    tooltipPlacement: Position
+    tooltipFormatter?: TooltipFormatter
+    focus: boolean
+    disabled: boolean
+  }>(),
+  { value: 0, focus: false, disabled: false },
+)
+const emit = defineEmits<{
+  (e: 'dragStart'): void
+}>()
+
+const showTooltip = computed(() => {
+  switch (props.tooltip) {
+    case 'always':
+      return true
+    case 'none':
+      return false
+    case 'focus':
+    case 'active':
+      return !!props.focus
+    default:
+      return false
+  }
+})
+
+const dotClasses = computed(() => [
+  'vue-slider-dot',
+  {
+    'vue-slider-dot-hover': props.tooltip === 'hover' || props.tooltip === 'active',
+    'vue-slider-dot-disabled': props.disabled,
+    'vue-slider-dot-focus': props.focus,
+  },
+])
+const handleClasses = computed(() => [
+  'vue-slider-dot-handle',
+  {
+    'vue-slider-dot-handle-disabled': props.disabled,
+    'vue-slider-dot-handle-focus': props.focus,
+  },
+])
+const tooltipClasses = computed(() => [
+  'vue-slider-dot-tooltip',
+  [`vue-slider-dot-tooltip-${props.tooltipPlacement}`],
+  {
+    'vue-slider-dot-tooltip-show': showTooltip.value,
+  },
+])
+const tooltipInnerClasses = computed(() => [
+  'vue-slider-dot-tooltip-inner',
+  [`vue-slider-dot-tooltip-inner-${props.tooltipPlacement}`],
+  {
+    'vue-slider-dot-tooltip-inner-disabled': props.disabled,
+    'vue-slider-dot-tooltip-inner-focus': props.focus,
+  },
+])
+
+const tooltipValue = computed(() => {
+  if (props.tooltipFormatter) {
+    return typeof props.tooltipFormatter === 'string'
+      ? props.tooltipFormatter.replace(/\{value\}/, String(props.value))
+      : props.tooltipFormatter(props.value)
+  } else {
+    return props.value
+  }
+})
+const dragStart = () => {
+  if (props.disabled) {
+    return false
+  }
+  emit('dragStart')
+}
+</script>
+
+<template>
+  <div
+    :class="dotClasses"
+    :aria-valuetext="tooltipValue"
+    @mousedown="dragStart"
+    @touchstart="dragStart"
+  >
+    <template v-if="$slots.dot()">
+      <slot name="dot" />
+    </template>
+    <template v-else>
+      <div :class="handleClasses" :style="tooltipStyle" />
+    </template>
+    <template v-if="tooltip !== 'none'">
+      <div :class="tooltipClasses">
+        <template v-if="$slots.tooltip()">
+          <slot name="tooltip" />
+        </template>
+        <template v-else>
+          <div :class="tooltipInnerClasses" :style="tooltipStyle">
+            <span class="vue-slider-dot-tooltip-text">{{ tooltipValue }}</span>
+          </div>
+        </template>
+      </div>
+    </template>
+  </div>
+</template>
